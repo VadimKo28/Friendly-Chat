16aed5a762024de725ef91af6c4264afc9eae9e2
Your Name <you@example.com>
2023-03-07 14:02:38 +0300

    Create rooms

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,32 @@
+class RoomsController < ApplicationController
+    before_action :set_room, only: %i[show update edit destroy]
+  def index
+    @rooms = Room.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_param)
+    if @room.save
+      redirect_to room_path(@room)
+    else
+      render :new
+    end
+  end
+
+  def show 
+  end
+
+  private
+
+  def room_param
+    params.require(:room).permit(:title)
+  end
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+end
