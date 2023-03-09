bf6710fbc8ac1dd042bcac0d56ff11942a9146ee
Your Name <you@example.com>
2023-03-09 19:47:55 +0300

    Add message controller, create message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root "rooms#index"
-  resources :rooms
+  resources :rooms do 
+    resources :messages
+  end
   get '/users/:id', to: 'users#show', as: "users"
 
 end
